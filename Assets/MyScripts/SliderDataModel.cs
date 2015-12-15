894ff26d95ce67c30e20e80f83775fd19a7ec1d7
edmundo096 <edmundo.geg@hotmail.com>
2015-12-15 18:51:29 +0100

    Upgrade main interior material. Remove camera from Simulator prefab. Create LeapWidgetsController gameobject. Organized hirarchy.

---
+++ Assets/MyScripts/SliderDataModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using LMWidgets;
+
+public class SliderDataModel : DataBinderSlider {
+    [SerializeField]
+    float sliderValue = 0; //Data model
+
+    override protected void setDataModel(float value) {
+        sliderValue = value;
+    }
+
+    override public float GetCurrentData() {
+        return sliderValue;
+    }
+}
---
+++ Assets/MyScripts/ToggleDataBinder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using LMWidgets;
+
+public class ToggleDataBinder : DataBinderToggle {
+  [SerializeField] 
+  Text uiText;
+
+  override public bool GetCurrentData() {
+    if ( uiText.text.ToLower() == "true" ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  override protected void setDataModel(bool value) { 
+    if ( value == true ) { 
+      uiText.text = "True";
+    }
+    else {
+      uiText.text = "False";
+    }
+  }
+}
